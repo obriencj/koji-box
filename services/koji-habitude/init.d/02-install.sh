1ac22e940fe8067feb0f5a3296bcd11c2c4b82b7
Christopher O'Brien <obriencj@gmail.com>
2025-10-16 20:10:43 -0400

    getting to a working koji

--- services/koji-habitude/init.d/02-install.sh
+++ services/koji-habitude/init.d/02-install.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
 
-python3 -m pip install --user --no-cache-dir /mnt/koji-habitude/
+python3 -m pip install --user PyYAML pydantic click
+
+python3 -m pip install --user --no-deps --force-reinstall \
+        /mnt/koji-habitude/dist/koji_habitude-0.1.0-py3-none-any.whl
 
 
 # The end.
